a26818e7c7ea8a7fe065182500bbe9c9dd2183e3
soursafyona <104200836+soursafyona@users.noreply.github.com>
2022-06-19 01:40:30 +0200

    Update assistant.py

--- handlers/assistant.py
+++ handlers/assistant.py
@@ -8,7 +8,7 @@ from callsmusic.callsmusic import client as user
 
 
 @Client.on_message(
-    command(["نضم", "assistant", "انضم" ,"تشغيل"]) & ~filters.private & ~filters.bot
+    command(["نضم", "assistant", " انضم" ,"بيمبو تعاله"]) & ~filters.private & ~filters.bot
 )
 @authorized_users_only
 async def join_chat(c: Client, m: Message):
@@ -23,7 +23,7 @@ async def join_chat(c: Client, m: Message):
             (await user.get_me()).id,
             can_manage_voice_chats=True
         )
-        return await user.send_message(chat_id, "🦦 تم انضمام الحساب المساعد.​")
+        return await user.send_message(chat_id, "🦦 تم انضمام الحساب المساعد")
     except UserAlreadyParticipant:
         admin = await m.chat.get_member((await user.get_me()).id)
         if not admin.can_manage_voice_chats:
@@ -31,5 +31,5 @@ async def join_chat(c: Client, m: Message):
                 (await user.get_me()).id,
                 can_manage_voice_chats=True
             )
-            return await user.send_message(chat_id, "🐰 موجود هنا منقبلك.​")
-        return await user.send_message(chat_id, "🐰 موجود هنا منقبلك.​")
+            return await user.send_message(chat_id, "🦦 موجود من زمان")
+        return await user.send_message(chat_id, "🦦 موجود من زمان​")
