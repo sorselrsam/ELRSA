bda3698204d54cde25084854401bfaddcf12d29a
soursafyona <104200836+soursafyona@users.noreply.github.com>
2022-06-19 21:43:54 +0200

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -14,7 +14,7 @@ def time_to_seconds(time):
     return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
 
 
-@Client.on_message(filters.command(["/song", "اغنيه" ,"فيديو"]) & ~filters.private & ~filters.channel)
+@Client.on_message(filters.command(["/song", "تحميل" ,"فيديو" ,"اغنيه"]) & ~filters.private & ~filters.channel)
 def song(client, message):
 
     message.delete()
