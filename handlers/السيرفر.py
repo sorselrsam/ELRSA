f97659e338e5aadaf6a04e22f8eb312044fb7d12
soursafyona <104200836+soursafyona@users.noreply.github.com>
2022-06-20 00:43:08 +0200

    Update السيرفر.py

--- handlers/السيرفر.py
+++ handlers/السيرفر.py
@@ -13,7 +13,7 @@ from driver.decorators import sudo_users_only, humanbytes
 
 # FETCH SYSINFO
 
-@Client.on_message(command2(["النظام","السيرفر","معلومات السيرفر"]))
+@Client.on_message(command2(["النظام" ,"السيرفر" ,"معلومات السيرفر"]))
 @sudo_users_only
 async def give_sysinfo(client, message):
     splatform = platform.system()
