b8df3899f3fab55c6b75f65554b6845a39772927
soursafyona <104200836+soursafyona@users.noreply.github.com>
2022-06-20 00:45:12 +0200

    Update قائمة.py

--- handlers/قائمة.py
+++ handlers/قائمة.py
@@ -15,19 +15,19 @@ from driver.filters import command2, other_filters
 
 
 keyboard = InlineKeyboardMarkup(
-    [[InlineKeyboardButton("🗑 اغلاق", callback_data="cls")]]
+    [[InlineKeyboardButton("🗑 ¦ اغلاق", callback_data="cls")]]
 )
 
 
-@Client.on_message(command2(["قائمة التشغيل","قائمه التشغيل","قايمة التشغيل","قايمه التشغيل"]) & other_filters)
+@Client.on_message(command2(["قائمة التشغيل" ,"قائمه التشغيل" ,"قايمة التشغيل" ,"قايمه التشغيل"]) & other_filters)
 async def playlist(client, m: Message):
    chat_id = m.chat.id
    if chat_id in QUEUE:
       chat_queue = get_queue(chat_id)
       if len(chat_queue)==1:
-         await m.reply(f"💡 **المشغل الان:**\n\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`", reply_markup=keyboard, disable_web_page_preview=True)
+         await m.reply(f"🐥 **المشغل الان:**\n\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`", reply_markup=keyboard, disable_web_page_preview=True)
       else:
-         QUE = f"💡 **المشغل الان:**\n\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}` \n\n**📖 قائمة الانتظار:**\n"
+         QUE = f"🐥 **المشغل الان:**\n\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}` \n\n**🦦 قائمة الانتظار:**\n"
          l = len(chat_queue)
          for x in range (1, l):
             han = chat_queue[x][0]
