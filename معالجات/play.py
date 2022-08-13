81139c5061b19f4fb176c14bc7fd13d5309a16cb
ELrasmElknk <110526070+ELrasmElknk@users.noreply.github.com>
2022-08-13 11:42:31 +0200

    Update and rename handlers/play.py to معالجات/play.py

--- معالجات/play.py
+++ معالجات/play.py
@@ -55,7 +55,7 @@ async def play(_, message: Message):
 
     await message.delete()
 
-    fallen = await message.reply("يتم التحميل \n\https://telegra.ph/file/6e65d72a52c1d987bcf68.mp4")
+    سقط  =  انتظر  الرسالة . رد ( "يتم التحميل \ n \💯 " )
 
     chumtiya = message.from_user.mention
 
@@ -157,7 +157,7 @@ async def play(_, message: Message):
             return await fallen.edit(
                 "عليك كتابة اسم الاغنيه او الرد ع ملف صوتي"
             )
-        await fallen.edit("https://telegra.ph/file/6e65d72a52c1d987bcf68.mp4")
+        انتظر  سقطت . تحرير ( "💯" )
         query = message.text.split(None, 1)[1]
         # print(query)
         try:
